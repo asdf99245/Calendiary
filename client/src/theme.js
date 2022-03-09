1e6dee19b480dbed3c82e2ae286e853a5da8b50f
asdf99245 <asdf99245@naver.com>
2022-03-09 20:29:57 +0900

    style: theme box shadow속성 추가

--- client/src/theme.js
+++ client/src/theme.js
@@ -52,6 +52,9 @@ const common = {
   boxShadow_2: `
     box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, 
     rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;`,
+  boxShadow_yellow: `
+    box-shadow: 0px 1px 8px #fdcb6e;
+  `,
 };
 
 const theme = {
