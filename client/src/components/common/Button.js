118c123f9d2c3362e09842c576559449f55b4b7e
asdf99245 <asdf99245@naver.com>
2022-03-09 17:23:47 +0900

    feat: 공통 button 컴포넌트 생성

---
+++ client/src/components/common/Button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ButtonTemplate = styled.button`
+  outline: none;
+  border: none;
+  background: ${({ theme }) => theme.colors.blue_2};
+  color: white;
+  padding: ${({ theme }) => theme.spaces.base};
+  ${({ theme }) => theme.common.boxShadow};
+  cursor: pointer;
+  &:active {
+    box-shadow: none;
+  }
+`;
+
+function Button({ children, ...props }) {
+  return <ButtonTemplate {...props}>{children}</ButtonTemplate>;
+}
+
+export default Button;
