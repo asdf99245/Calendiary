df46dcb00b0b8624e8b288a710365e12f1293edd
asdf99245 <asdf99245@naver.com>
2022-03-09 16:00:02 +0900

    style: theme flexCenterColumn 속성 추가

--- client/src/theme.js
+++ client/src/theme.js
@@ -40,6 +40,11 @@ const common = {
     align-items: center;
     justify-content: center;
   `,
+  flexCenterColumn: `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
   boxShadow: `
     box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
     rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
