e66065cea3395381566c545576c343d0ba3d0a7a
asdf99245 <asdf99245@naver.com>
2022-03-21 02:22:39 +0900

    feat: 유저권한 검증 미들웨어 추가

---
+++ middlewares/authCheck.js
@@ -0,0 +1,16 @@
+const { verifyToken } = require('../utils/jwt');
+
+exports.authCheck = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split('Bearer ')[1];
+    const decoded = verifyToken(token, process.env.JWT_SECRET_KEY);
+    if (decoded) {
+      req.decoded = decoded;
+      next();
+    } else {
+      res.status(401).send('unauthorized');
+    }
+  } catch (err) {
+    res.send(401).send('token expired');
+  }
+};
