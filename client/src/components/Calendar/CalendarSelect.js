d00cfcaa1db57b7d87430db9576edbd54350f959
asdf99245 <asdf99245@naver.com>
2022-05-30 17:48:51 +0900

    feat: Select 메뉴 선택 year 제한 및 백그라운드 클릭시 닫힘

--- client/src/components/Calendar/CalendarSelect.js
+++ client/src/components/Calendar/CalendarSelect.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styled, { css } from 'styled-components';
+import Button from './../common/Button';
+import dayjs from 'dayjs';
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  z-index: 5;
+`;
 
 const SelectWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 200px;
   ${({ theme }) => theme.common.boxShadow_3};
   border-radius: ${({ theme }) => theme.borderRadius.base};
   position: absolute;
@@ -21,9 +32,9 @@ const SelectWrapper = styled.div`
 
 const Select = styled.div`
   display: flex;
-  height: 100%;
+  width: 250px;
   overflow: hidden;
-  margin-top: ${({ theme }) => theme.spaces.small};
+  margin-top: ${({ theme }) => theme.spaces.base};
 `;
 
 const List = styled.ul`
@@ -39,6 +50,7 @@ const List = styled.ul`
 const ListItem = styled.li`
   padding: 4px 6px;
   cursor: pointer;
+  text-align: center;
   &:hover {
     background-color: ${({ theme }) => theme.colors.gray_1};
   }
@@ -46,8 +58,15 @@ const ListItem = styled.li`
   ${(props) =>
     props.active &&
     css`
-      background-color: ${({ theme }) => theme.colors.gray_1};
+      background-color: ${({ theme }) => theme.colors.gray_3};
     `}
+`;
+
+const StyledButton = styled(Button)`
+  align-self: flex-end;
+  padding: ${({ theme }) => theme.spaces.small}
+    ${({ theme }) => theme.spaces.lg};
+  margin-top: ${({ theme }) => theme.spaces.base};
 `;
 
 function CalendarSelect({ currentDate, onClose, onSetDay }) {
@@ -55,61 +74,59 @@ function CalendarSelect({ currentDate, onClose, onSetDay }) {
     year: currentDate.get('year'),
     month: currentDate.get('month') + 1,
   });
-  const months = Array.from({ length: 12 }, (x, i) => i + 1);
-  const years = Array.from({ length: 123 }, (x, i) => i + 1900);
-  const monthRef = useRef();
-  const yearRef = useRef();
   const { year, month } = date;
 
+  const currentYear = dayjs().get('year');
+  const months = Array.from({ length: 12 }, (x, i) => i + 1);
+  const years = Array.from(
+    { length: currentYear - 1900 + 1 },
+    (x, i) => i + 1900
+  );
+  const monthRef = useRef();
+  const yearRef = useRef();
+
   useEffect(() => {
-    monthRef.current.scrollTop = 25 * (month - 1);
+    monthRef.current.scrollTop = 25 * (month - 1); // 25 => list item의 height
     yearRef.current.scrollTop = 25 * (year - 1901);
   }, []);
 
-  const onClickYear = (y) => {
+  const onClickDate = (e) => {
+    const { id, value } = e.target;
     setDate({
       ...date,
-      year: y,
+      [id]: value,
     });
   };
 
-  const onClickMonth = (m) => {
-    onSetDay(year, m);
+  const onClickButton = () => {
+    onSetDay(year, month);
     onClose();
   };
 
   return (
-    <SelectWrapper>
-      <h2>날짜 선택</h2>
-      <Select>
-        <List ref={yearRef}>
-          {years.map((y, i) => (
-            <ListItem
-              key={i}
-              active={year === y}
-              id="year"
-              value={y}
-              onClick={() => onClickYear(y)}
-            >
-              {y}
-            </ListItem>
-          ))}
-        </List>
-        <List ref={monthRef}>
-          {months.map((m, i) => (
-            <ListItem
-              key={i}
-              active={month === m}
-              id="month"
-              value={m}
-              onClick={() => onClickMonth(m)}
-            >
-              {m}
-            </ListItem>
-          ))}
-        </List>
-      </Select>
-    </SelectWrapper>
+    <>
+      <Background onClick={onClose} />
+      <SelectWrapper>
+        <h2>날짜</h2>
+        <Select onClick={onClickDate}>
+          <List ref={yearRef}>
+            {years.map((y, i) => (
+              <ListItem key={i} active={year === y} id="year" value={y}>
+                {y}
+              </ListItem>
+            ))}
+          </List>
+          <List ref={monthRef}>
+            {months.map((m, i) => (
+              <ListItem key={i} active={month === m} id="month" value={m}>
+                {m}
+              </ListItem>
+            ))}
+          </List>
+        </Select>
+        <StyledButton onClick={onClickButton}>GO</StyledButton>
+      </SelectWrapper>
+    </>
   );
 }
 
