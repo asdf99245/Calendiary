7fda4f99047b380a1e0aa892b2ccc16f17051d05
asdf99245 <asdf99245@naver.com>
2022-11-05 21:20:00 +0900

    Feat: express 오류처리 미들웨어 추가

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-const api = require('./routes/api');
-const { sequelize } = require('./database/models');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
+const api = require('./routes/api');
+const { sequelize } = require('./database/models');
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(
   cors({
@@ -25,6 +25,17 @@ sequelize
 app.use('/uploads', express.static('./uploads'));
 
 app.use('/api', api);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const statusCode = err.status || 500;
+
+  res.status(statusCode).json({
+    success: false,
+    message: err.message,
+  });
+});
 
 app.listen(port, () => {
   const dir = './uploads';
