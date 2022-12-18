ba46fbc4c541b0563558155af1451c55c962dc06
asdf99245 <asdf99245@naver.com>
2022-12-18 18:50:53 +0900

    refactor: auth 컨트롤러 로직을 서비스 및 레포지토리로 분리

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,112 +1,65 @@
-const { User } = require('../database/models');
-const { Op } = require('sequelize');
-const {
-  verifyToken,
-  makeAccessToken,
-  makeRefreshToken,
-} = require('../utils/jwt');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const authService = require('../services/auth.service');
+const userService = require('../services/user.service');
+const { makeAccessToken, makeRefreshToken } = require('../utils/jwt');
+const STATUS_CODE = require('../utils/statusCode');
 
 module.exports = {
-  silentRefresh: async (req, res) => {
-    const { refreshToken } = req.cookies;
-    if (refreshToken) {
-      const verifyRefreshToken = verifyToken(refreshToken);
-      if (verifyRefreshToken.id) {
-        id = verifyRefreshToken.id;
-        const accessToken = makeAccessToken(id);
-        const refreshToken = makeRefreshToken(id);
-        try {
-          const result = await User.findOne({ where: { user_id: id } });
-          const { user_id, user_name } = result;
-          res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-          });
-          res.json({
-            success: true,
-            status: 200,
-            accessToken,
-            user_id,
-            user_name,
-          });
-        } catch (err) {
-          throw err;
-        }
-      }
-    } else {
-      res.status(401).send('Refresh failed');
+  silentRefresh: async (req, res, next) => {
+    try {
+      const id = await authService.checkRefreshToken(req.cookies.refreshToken);
+      const accessToken = makeAccessToken(id);
+      const refreshToken = makeRefreshToken(id);
+
+      const { user_id, user_name } = userService.findOneByFilter({
+        user_id: id,
+      });
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+      });
+      res.status(STATUS_CODE.OK).json({
+        accessToken,
+        user_id,
+        user_name,
+      });
+    } catch (err) {
+      next(err);
     }
   },
-  login: async (req, res) => {
+  login: async (req, res, next) => {
     const { user_id, user_password } = req.body;
     try {
-      const result = await User.findOne({ where: { user_id } });
-      if (!result) {
-        res.json({
-          success: false,
-          status: 404,
-          message: '존재하지 않는 계정입니다.',
-        });
-      } else {
-        const ok = await bcrypt.compare(user_password, result.user_password); // bcrypt를 통한 비밀번호 비교
-        if (ok) {
-          const accessToken = makeAccessToken(user_id);
-          const refreshToken = makeRefreshToken(user_id);
-          res.cookie('accessToken', accessToken);
-          res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-          });
-          res.json({
-            success: true,
-            status: 200,
-            message: '로그인에 성공하였습니다.',
-            accessToken,
-            user_id,
-            user_name: result.user_name,
-          });
-        } else {
-          res.json({
-            success: false,
-            status: 401,
-            message: '비밀번호가 일치하지 않습니다.',
-          });
-        }
-      }
+      const user = await authService.signIn(user_id, user_password);
+      const accessToken = makeAccessToken(user_id);
+      const refreshToken = makeRefreshToken(user_id);
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+      });
+
+      res.status(STATUS_CODE.OK).json({
+        message: '로그인에 성공하였습니다.',
+        user_id,
+        user_name: user.user_name,
+        accessToken,
+      });
     } catch (err) {
-      throw err;
+      next(err);
     }
   },
   logout: (req, res) => {
-    res.clearCookie('accessToken');
     res.clearCookie('refreshToken');
-    res.json({
-      success: true,
-      status: 200,
+    res.status(STATUS_CODE.OK).json({
       message: '로그아웃에 성공하였습니다.',
     });
   },
-  register: async (req, res) => {
+  register: async (req, res, next) => {
     const { user_id, user_password, user_name } = req.body;
     try {
-      const result = await User.findOne({ where: { user_id } });
-      if (result) {
-        res.json({
-          success: false,
-          status: 409,
-          message: '이미 존재하는 아이디입니다.',
-        });
-      } else {
-        const encrypted = await bcrypt.hash(user_password, saltRounds);
-        await User.create({ user_id, user_password: encrypted, user_name });
-        res.json({
-          success: true,
-          status: 201,
-          message: '회원가입이 성공적으로 완료되었습니다.',
-        });
-      }
+      await authService.signUp(user_id, user_password, user_name);
+      res.status(STATUS_CODE.CREATED).json({
+        message: '회원가입이 성공적으로 완료되었습니다.',
+      });
     } catch (err) {
-      throw err;
+      next(err);
     }
   },
 };
