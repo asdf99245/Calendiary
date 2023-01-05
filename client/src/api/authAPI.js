7e4d3c6fc8946c251f5ada073d9fc86a612c231f
asdf99245 <asdf99245@naver.com>
2023-01-05 16:06:35 +0900

    refactor: 기존 axios에서 axios instance 모듈 사용

--- client/src/api/authAPI.js
+++ client/src/api/authAPI.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
+import axios from './axiosInstance';
 
 export const silentRefresh = async () => {
-  const response = await axios.get('/api/auth/silent-refresh');
+  const response = await axios.get('/auth/silent-refresh');
   return response;
 };
 
 export const onLogin = async (infos) => {
-  const response = await axios.post('/api/auth/login', infos);
+  const response = await axios.post('/auth/login', infos);
   return response;
 };
 
 export const onLogout = async () => {
-  const response = await axios.post('/api/auth/logout', {});
+  const response = await axios.post('/auth/logout');
   return response;
 };
 
 export const onRegister = async (infos) => {
-  const response = await axios.post('/api/auth/register', infos);
+  const response = await axios.post('/auth/register', infos);
   return response;
 };
